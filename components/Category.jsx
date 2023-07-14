c0828e813862d5784ad5a1a298fdee1d7b61584f
mybess00 <betancourtrafael00@gmail.com>
2023-07-14 17:04:49 -0400

    update

--- components/Category.jsx
+++ components/Category.jsx
@@ -1,6 +1,6 @@
 export default function Category ({ title, id, children }) {
   return (
-    <div className="main-container-category" id={id}>
+    <div className="main-container-category" id={id} key={id}>
       <div className="shape-category">{title}</div>
       <div className="category-container"> 
         {children}
